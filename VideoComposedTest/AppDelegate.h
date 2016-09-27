f825ce2f2feee23210fe4231637ae26a8751b869
qianlishun <511940597@qq.com>
2016-09-27 16:10:51 +0800

    Initial Commit

---
+++ VideoComposedTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoComposedTest
+//
+//  Created by mrq on 16/9/27.
+//  Copyright © 2016年 Sonoptek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoComposedTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoComposedTest
+//
+//  Created by mrq on 16/9/27.
+//  Copyright © 2016年 Sonoptek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
